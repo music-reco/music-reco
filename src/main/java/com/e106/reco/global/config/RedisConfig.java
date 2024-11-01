335a686b68cfd70d5eb1670be52f78466a562c8a
송도언 <ehdjs9583@naver.com>
2024-11-01 12:21:01 +0900

    [BE] redis config log

--- src/main/java/com/e106/reco/global/config/RedisConfig.java
+++ src/main/java/com/e106/reco/global/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.ee06.wooms.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
+@Slf4j
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
@@ -26,6 +28,9 @@ public class RedisConfig {
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
         config.setPassword(password);
+        log.info("host : {}", host);
+        log.info("port : {}", port);
+        log.info("password : {}", password);
         return new LettuceConnectionFactory(config);
     }
 
