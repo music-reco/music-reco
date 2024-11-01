7fe4efe1dd744c0331aba08232049b48c8a6c917
송도언 <ehdjs9583@naver.com>
2024-11-01 11:49:39 +0900

    [BE] corsFilter 수정

--- src/main/java/com/e106/reco/global/config/CorsConfig.java
+++ src/main/java/com/e106/reco/global/config/CorsConfig.java
@@ -20,7 +20,6 @@ public class CorsConfig {
     //    config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://i11e201.p.ssafy.io"));
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("https://k11e106.p.ssafy.io");
-        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.setExposedHeaders(Arrays.asList("Authorization"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
