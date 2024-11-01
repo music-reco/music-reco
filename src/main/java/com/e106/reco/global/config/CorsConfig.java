e55067aac8a3f86ebca6f94bca800067a97c1103
송도언 <ehdjs9583@naver.com>
2024-11-01 17:21:10 +0900

    Update src/main/java/com/e106/reco/global/config/CorsConfig.java

--- src/main/java/com/e106/reco/global/config/CorsConfig.java
+++ src/main/java/com/e106/reco/global/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
         config.setAllowCredentials(true);
     //    config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://i11e201.p.ssafy.io"));
-        // config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("https://k11e106.p.ssafy.io");
         config.addAllowedHeader("*");
         config.setExposedHeaders(Arrays.asList("Authorization"));
