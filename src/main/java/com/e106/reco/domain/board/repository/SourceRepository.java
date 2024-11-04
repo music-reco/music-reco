d1529f62bc8dbb5e53902b1c08663485a2d68105
송도언 <ehdjs9583@naver.com>
2024-11-04 09:50:37 +0900

    Revert "Merge branch 'Refactor#13' into 'master'"
    This reverts merge request !22

--- src/main/java/com/e106/reco/domain/board/repository/SourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.e106.reco.domain.board.repository;
-
-import com.e106.reco.domain.board.entity.Source;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SourceRepository extends JpaRepository<Source, Long> {
-}
--- src/main/java/com/e106/reco/global/s3/ThumbnailSize.java
+++
@@ -1,6 +0,0 @@
-package com.e106.reco.global.s3;
-
-public interface ThumbnailSize {
-    int WIDTH = 200;
-    int HEIGHT = 200;
-}
