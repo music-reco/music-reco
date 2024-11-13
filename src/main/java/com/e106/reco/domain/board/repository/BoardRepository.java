4d6174331d4cb0a70c6bce969dedd9a6d4e53c65
권혜경 <skygprud0707@gmail.com>
2024-11-13 10:59:52 +0900

    feat : 게시글 삭제 수정

--- src/main/java/com/e106/reco/domain/board/repository/BoardRepository.java
+++ src/main/java/com/e106/reco/domain/board/repository/BoardRepository.java
@@ -1,7 +1,6 @@
 package com.e106.reco.domain.board.repository;
 
 import com.e106.reco.domain.board.entity.Board;
-import com.e106.reco.domain.board.entity.BoardState;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,9 +14,13 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
             "AND b.seq = :seq")
     Optional<Board> findBySeq(@Param("seq")Long seq);
 
+    @Query("select b from Board b where b.state != com.e106.reco.domain.board.entity.BoardState.INACTIVE " +
+            "AND b.artist.seq = :artist_seq")
     List<Board> findByArtist_seq(Long artist_seq, Pageable pageable);
 
-    List<Board> findByArtist_seqAndState(Long artist_seq, BoardState state, Pageable pageable);
+    @Query("select b from Board b where b.state = com.e106.reco.domain.board.entity.BoardState.PUBLIC " +
+            "AND b.artist.seq = :artist_seq")
+    List<Board> findPublicBoardByArtist_seq(Long artist_seq, Pageable pageable);
 
 
 }
