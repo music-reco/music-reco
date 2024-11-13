731faf9222e6eeb2b77701c12913bf5d58f83a29
Songdoeon <ehdjs9583@naver.com>
2024-11-13 19:01:04 +0900

    [BE] init

--- src/main/java/com/e106/reco/domain/workspace/dto/divide/CancelResponse.java
+++
@@ -1,9 +0,0 @@
-package com.e106.reco.domain.workspace.dto.divide;
-
-import lombok.Data;
-
-@Data
-public class CancelResponse {
-    private String status;
-    private String error;
-}
--- src/main/java/com/e106/reco/domain/workspace/entity/converter/SplitterType.java
+++
@@ -1,5 +0,0 @@
-package com.e106.reco.domain.workspace.entity.converter;
-
-public enum SplitterType {
-    PHOENIX, ORION, PERSEUS
-}
