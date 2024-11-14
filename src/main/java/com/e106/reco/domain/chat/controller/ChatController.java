cf3ed4bfca8c331a9806b98fc0a9b439c6264c8e
Songdoeon <ehdjs9583@naver.com>
2024-11-14 14:08:36 +0900

    [BE] webflux url 제거

--- src/main/java/com/e106/reco/domain/chat/controller/ChatController.java
+++ src/main/java/com/e106/reco/domain/chat/controller/ChatController.java
@@ -1,24 +1,16 @@
 package com.e106.reco.domain.chat.controller;
 
 import com.e106.reco.domain.chat.dto.RoomRequest;
-import com.e106.reco.domain.chat.dto.RoomResponse;
-import com.e106.reco.domain.chat.entity.Chat;
-import com.e106.reco.domain.chat.entity.ChatArtist;
 import com.e106.reco.domain.chat.repository.ChatRepository;
 import com.e106.reco.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -47,16 +39,16 @@ public class ChatController {
 //        return
     }
 
-    @PostMapping("/webflux")
-    public Mono<Chat> postMsg(@RequestBody Chat chat) {
-        return chatService.sendMsg(chat);
-    }
-
-
-    @GetMapping(value = "/webflux/artistInfo/{roomSeq}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ChatArtist> getArtistInfo(@PathVariable("roomSeq") Long roomSeq) {
-        return chatService.getArtistInfo(roomSeq);
-    }
+//    @PostMapping("/webflux")
+//    public Mono<Chat> postMsg(@RequestBody Chat chat) {
+//        return chatService.sendMsg(chat);
+//    }
+//
+//
+//    @GetMapping(value = "/webflux/artistInfo/{roomSeq}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<ChatArtist> getArtistInfo(@PathVariable("roomSeq") Long roomSeq) {
+//        return chatService.getArtistInfo(roomSeq);
+//    }
 
 
 
@@ -67,15 +59,15 @@ public class ChatController {
 //        return chatService.findByPk(groupSeq);
 //    }
 
-    @GetMapping(value = "/webflux/{roomSeq}/{artistSeq}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Chat> getMsg(@PathVariable("artistSeq")Long artistSeq, @PathVariable("roomSeq")String roomSeq) {
-        return chatRepository.mFindByRoomSeq(roomSeq).subscribeOn(Schedulers.boundedElastic());
-    }
-
-    @GetMapping(value = "/webflux/{artistSeq}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public  Flux<RoomResponse> getChatRooms(@PathVariable("artistSeq")Long artistSeq) {
-        return chatService.getChatRooms(artistSeq);
-    }
+//    @GetMapping(value = "/webflux/{roomSeq}/{artistSeq}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<Chat> getMsg(@PathVariable("artistSeq")Long artistSeq, @PathVariable("roomSeq")String roomSeq) {
+//        return chatRepository.mFindByRoomSeq(roomSeq).subscribeOn(Schedulers.boundedElastic());
+//    }
+//
+//    @GetMapping(value = "/webflux/{artistSeq}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public  Flux<RoomResponse> getChatRooms(@PathVariable("artistSeq")Long artistSeq) {
+//        return chatService.getChatRooms(artistSeq);
+//    }
 
 
 //    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
