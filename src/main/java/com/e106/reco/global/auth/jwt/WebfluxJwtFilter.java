a18557639bfbf757aa256393be038c5bba48c772
KHG <skygprud0707@gmail.com>
2024-11-14 14:57:49 +0900

    feat : 401 error

--- src/main/java/com/e106/reco/global/auth/jwt/WebfluxJwtFilter.java
+++ src/main/java/com/e106/reco/global/auth/jwt/WebfluxJwtFilter.java
@@ -10,7 +10,6 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -56,7 +55,6 @@ public class WebfluxJwtFilter implements WebFilter {
         }
 
         Authentication authToken = createAuthentication(token);
-        SecurityContextHolder.getContext().setAuthentication(authToken);
 
         return chain.filter(exchange)
                 .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authToken));
