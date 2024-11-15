74d4b394b10c5173c1dd1b18fad3bd97f9cc9aeb
KHG <skygprud0707@gmail.com>
2024-11-15 17:07:52 +0900

    feat : 가입일자 기반 채팅 목록 데이터 받아오기

--- src/main/java/com/e106/reco/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/e106/reco/domain/chat/repository/ChatRoomRepository.java
@@ -30,7 +30,7 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long>{
     @Query("select cr.room from ChatRoom cr where cr.artist.seq = :artistSeq AND cr.state != com.e106.reco.domain.chat.entity.RoomState.INACTIVE")
     List<Room> findExistRoomsByArtistSeq(@Param("artistSeq") Long artistSeq);
 
-    @Query("select cr.artist from ChatRoom cr where cr.room.seq = :roomSeq AND cr.state != com.e106.reco.domain.chat.entity.RoomState.INACTIVE")
+    @Query("select cr.artist from ChatRoom cr where cr.room.seq = :roomSeq AND cr.state != com.e106.reco.domain.chat.entity.RoomState.INACTIVE AND cr.joinAt is not null")
     List<Artist> findExistNameRoomsByRoomSeq(@Param("roomSeq") Long roomSeq);
 
     @Query("select cr.joinAt from ChatRoom cr where cr.pk = :pk")
