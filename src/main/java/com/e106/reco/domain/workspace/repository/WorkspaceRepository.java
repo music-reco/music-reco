57195b1c9b72a47da285c589595703b3ea596e53
Songdoeon <ehdjs9583@naver.com>
2024-11-15 14:47:36 +0900

    [BE] 레포지토리 추가

--- src/main/java/com/e106/reco/domain/workspace/repository/WorkspaceRepository.java
+++ src/main/java/com/e106/reco/domain/workspace/repository/WorkspaceRepository.java
@@ -7,8 +7,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
 
     @Query("SELECT w FROM Workspace w WHERE w.artist.seq = :artistSeq AND w.state != :state")
     Page<Workspace> findByArtistSeqAndStateNot(Long artistSeq, WorkspaceState state, Pageable pageable);
+
+    @Query("SELECT w FROM Workspace w JOIN FETCH w.artist a " +
+            "WHERE w.seq = :artistSeq")
+    Optional<Workspace> findByArtistSeq(Long artistSeq);
 }
