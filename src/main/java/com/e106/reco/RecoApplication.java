fb6078d0cc525251bb4b0ed09df11e13aee72bcc
Songdoeon <ehdjs9583@naver.com>
2024-11-15 17:05:11 +0900

    {BE] backend 서버 시간대 설정

--- src/main/java/com/e106/reco/RecoApplication.java
+++ src/main/java/com/e106/reco/RecoApplication.java
@@ -1,12 +1,20 @@
 package com.e106.reco;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import java.util.TimeZone;
+
 @EnableMongoRepositories
 @SpringBootApplication
 public class RecoApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
 
