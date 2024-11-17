72c0c61d92d9aacf79e638f2fc1d7b15db99d4f9
Songdoeon <ehdjs9583@naver.com>
2024-11-17 21:27:43 +0900

    [BE] neo4jTransactionManager bean 네임

--- src/main/java/com/e106/reco/global/config/Neo4jConfig.java
+++ src/main/java/com/e106/reco/global/config/Neo4jConfig.java
@@ -4,17 +4,11 @@ import org.neo4j.driver.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Configuration
 public class Neo4jConfig {
 
-    @Bean
-    public TransactionTemplate neo4jTransactionTemplate(Neo4jTransactionManager neo4jTransactionManager) {
-        return new TransactionTemplate(neo4jTransactionManager);
-    }
-
-    @Bean
+    @Bean(name = "neo4jTransactionManager")
     public Neo4jTransactionManager neo4jTransactionManager(Driver driver) {
         return new Neo4jTransactionManager(driver);
     }
