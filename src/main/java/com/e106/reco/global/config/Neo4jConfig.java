08b68c3ae3b86be99a3b17f970bd250408391937
Songdoeon <ehdjs9583@naver.com>
2024-11-17 21:37:39 +0900

    [BE] jpaTransaction 선언

--- src/main/java/com/e106/reco/global/config/Neo4jConfig.java
+++ src/main/java/com/e106/reco/global/config/Neo4jConfig.java
@@ -1,15 +1,28 @@
 package com.e106.reco.global.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.neo4j.driver.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class Neo4jConfig {
 
     @Bean(name = "neo4jTransactionManager")
     public Neo4jTransactionManager neo4jTransactionManager(Driver driver) {
         return new Neo4jTransactionManager(driver);
+    }
+    // JPA 트랜잭션 매니저
+    @Bean(name = "jpaTransactionManager")
+    @Primary  // 기본 트랜잭션 매니저로 설정
+    public PlatformTransactionManager transactionManager(
+            EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
 }
